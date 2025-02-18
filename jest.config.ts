bf84ac44cce659fa3644d0213235a896ca68ffc8
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-02-18 18:42:12 +0300

    test: edit jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,26 @@
 import nextJest from 'next/jest';
+import type { Config } from 'jest';
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
-const customJestConfig = {
+const customJestConfig: Config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!jest.config.ts',
+    '!middleware.ts',
+    '!next.config.ts',
+    '!lib/http/**',
+    '!lib/prisma.ts',
+    '!lib/session.ts',
+  ],
 };
 
 export default createJestConfig(customJestConfig);
