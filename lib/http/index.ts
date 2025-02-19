444e14c05622d356f551ef34ece711c1c1a4a316
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-02-20 00:55:48 +0300

    fix: api url

--- lib/http/index.ts
+++ lib/http/index.ts
@@ -1,8 +1,6 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
-
 type ReturnType = Promise<Response>;
 
 export async function fetchFromAPI(endpoint: string, init?: RequestInit): ReturnType {
-  const url = `${API_BASE_URL}/api/${endpoint}`;
+  const url = `/api/${endpoint}`;
   return await fetch(url, init);
 }
