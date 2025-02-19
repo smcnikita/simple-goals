3b9842a7a6882b9207b78c7c28c3ae206e4fc2da
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-02-20 01:13:18 +0300

    aside toggle

--- components/ui/header/components/HeaderMenu.tsx
+++ components/ui/header/components/HeaderMenu.tsx
@@ -1,11 +1,17 @@
-import type { FC } from 'react';
+'use client';
+
+import { useMemo, type FC } from 'react';
 import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 
 import { PATHS } from '@/constants/paths';
+
+import BaseIcon, { MenuDuoIcon } from '../../icon';
 
 import HeaderMenuItem from './HeaderMenuItem';
 
 import classes from '../styles/header.module.css';
+import classesAside from '@/components/ui/aside/styles/aside.module.css';
 
 type Props = {
   isAuth: boolean;
@@ -13,11 +19,33 @@ type Props = {
 
 const HeaderMenu: FC<Props> = ({ isAuth }) => {
   const t = useTranslations('NavLinks');
+  const pathname = usePathname();
 
   const nowYear = new Date().getFullYear();
 
+  const showMenuDuo = useMemo(() => {
+    return pathname.includes(PATHS.goals.base);
+  }, [pathname]);
+
+  const onToggleAside = () => {
+    const aside = document.getElementById('aside');
+
+    if (!aside) {
+      return;
+    }
+
+    aside.classList.toggle(classesAside.aside__open);
+  };
+
   return (
     <nav className={classes.tabs}>
+      {showMenuDuo && (
+        <button type="button" className={classes.asideToggle} onClick={onToggleAside}>
+          <BaseIcon>
+            <MenuDuoIcon />
+          </BaseIcon>
+        </button>
+      )}
       <HeaderMenuItem href={PATHS.home}>{t('home')}</HeaderMenuItem>
       {isAuth && (
         <HeaderMenuItem href={PATHS.goals.base + PATHS.goals.slug.replace(':slug', nowYear.toString())}>
---
+++ components/ui/icon/components/MenuDuoIcon.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+const MenuDuoIcon: FC = () => {
+  return (
+    <path d="M5 15H19M5 9H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+  );
+};
+
+export default MenuDuoIcon;
