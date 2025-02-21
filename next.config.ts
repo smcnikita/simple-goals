ef5f13b82c8fd73d6b10f3b87eb8ebea30b8acb2
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-02-21 11:30:20 +0300

    feat: disabled home page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+import { PATHS } from '@/constants/paths';
+
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async redirects() {
+    const nowYear = new Date().getFullYear();
+
+    return [
+      {
+        source: '/',
+        destination: PATHS.goals.base + PATHS.goals.slug.replace(':slug', nowYear.toString()),
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
