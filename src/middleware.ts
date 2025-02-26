e6be5e2c6ee8236dd21aef8cdcfb180997ba2bcc
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-02-26 14:28:37 +0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,13 @@ import { PATHS } from './constants/paths';
 
 export default withAuth({
   callbacks: {
-    authorized: ({ token }) => {
-      // Если токен существует, пользователь авторизован
-      return !!token;
-    },
+    authorized: ({ token }) => !!token,
   },
   pages: {
     signIn: PATHS.auth.signIn,
   },
 });
-// Конфигурация middleware
+
 export const config = {
   matcher: ['/goals/:path*'],
 };
