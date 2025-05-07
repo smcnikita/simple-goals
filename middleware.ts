acb993e42e630d33e2dff307d66cc4bfd247a93e
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-05-07 15:31:00 +0300

    feat: change index page

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server';
 
 export default withAuth(
   function middleware(req) {
-    // const nowYear = new Date().getFullYear();
-    // if (req.nextUrl.pathname === '/') {
-    // return NextResponse.redirect(new URL('/goals' + `/${nowYear}`, req.url));
-    // }
+    const nowYear = new Date().getFullYear();
+    if (req.nextUrl.pathname === '/') {
+      return NextResponse.redirect(new URL('/goals' + `/${nowYear}`, req.url));
+    }
     return NextResponse.next();
   },
   {
