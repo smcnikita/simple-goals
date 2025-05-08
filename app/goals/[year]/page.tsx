e851e1b89dd59d8e3870da60776539438669c9f0
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-05-08 20:50:48 +0300

    chore: code optimization

--- app/goals/[year]/page.tsx
+++ app/goals/[year]/page.tsx
@@ -1,30 +1,67 @@
-import type { FC } from 'react';
+import { notFound } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
-import Goals from '@/components/goals/Goals';
+import { authOptions } from '@/lib/auth';
 
 import { getStatuses } from '@/services/statuses-service';
 
-import PageProvider from './_page-provider';
+import { yearsController } from '@/controllers/years-controller';
 
-type Params = Promise<{
-  year: string;
-}>;
+import StoreInitializer from './store-initializer';
 
-type Props = {
-  params: Params;
-};
+import Goals from '@/components/goals/Goals';
 
-const GoalsPage: FC<Props> = async ({ params }) => {
-  const yearSlugNumber = Number((await params).year);
+async function validateYear(inputYear: string): Promise<boolean> {
+  const isValidFormat = /^\d{4}$/.test(inputYear);
+
+  if (!isValidFormat) {
+    return false;
+  }
+
+  const currentYear = new Date().getFullYear();
+  const MIN_VALID_YEAR = 2020;
+  const numericYear = Number(inputYear);
+
+  if (numericYear < MIN_VALID_YEAR || numericYear > currentYear) {
+    return false;
+  }
+
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) {
+    return false;
+  }
+
+  const userId = Number(session.user.id);
+
+  try {
+    const availableYears = await yearsController.getYears({ userId });
+
+    return availableYears.some(({ year }) => year === numericYear);
+  } catch (error) {
+    console.error('Error fetching available years:', error);
+    return false;
+  }
+}
+
+async function GoalsPage({ params }: { params: Promise<{ year: string }> }) {
+  const yearSlug = (await params).year;
+
+  const isValidYear = await validateYear(yearSlug);
+
+  if (!isValidYear) {
+    notFound();
+  }
+
   const statuses = await getStatuses();
 
   return (
-    <PageProvider statuses={statuses}>
+    <StoreInitializer statuses={statuses}>
       <div className="flex flex-col gap-6">
-        <Goals globalYear={yearSlugNumber} />
+        <Goals globalYear={Number(yearSlug)} />
       </div>
-    </PageProvider>
+    </StoreInitializer>
   );
-};
+}
 
 export default GoalsPage;
---
+++ app/goals/[year]/store-initializer.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useMemo, type FC, type PropsWithChildren } from 'react';
+
+import { STATUS_TOTAL } from '@/constants/statuses';
+
+import { useFilterStatusStore } from '@/stores/filter-status-store';
+import { useStatusStore } from '@/stores/status-store';
+
+import type { Statuses } from '@/types/statuses.types';
+
+type Props = {
+  statuses: Statuses;
+};
+
+const StoreInitializer: FC<PropsWithChildren<Props>> = ({ children, statuses }) => {
+  const { updateStatusOptions } = useStatusStore();
+  const { updateFilterStatusOptions } = useFilterStatusStore();
+
+  const statusOptions = useMemo(() => statuses.map(({ key, name }) => ({ key, name })), [statuses]);
+
+  const filterStatusOptions = useMemo(
+    () => [{ key: STATUS_TOTAL.key, name: STATUS_TOTAL.name }, ...statusOptions],
+    [statusOptions]
+  );
+
+  useEffect(() => {
+    updateStatusOptions(statusOptions);
+    updateFilterStatusOptions(filterStatusOptions);
+  }, [statusOptions, filterStatusOptions, updateStatusOptions, updateFilterStatusOptions]);
+
+  return children;
+};
+
+export default StoreInitializer;
