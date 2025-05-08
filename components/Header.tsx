07dba12fe34a82c90c731699015abe9cea984e94
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-05-08 21:35:06 +0300

    feat: add i18n

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { User2, LogOut } from 'lucide-react';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
@@ -16,9 +16,22 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { type Locale, locales, localesInfo } from '@/i18n/config';
+import { setUserLocale } from '@/services/locale-service';
+import { useLocale } from 'next-intl';
 
 const Header: FC = () => {
   const { data: session } = useSession();
+  const locale = useLocale() as Locale;
+
+  const [langValue, setLangValue] = useState<Locale>(locale);
+
+  const onChangeLang = (value: Locale) => {
+    setLangValue(value);
+    setUserLocale(value);
+  };
 
   return (
     <header className="py-4 px-3 flex items-center justify-between gap-1">
@@ -29,27 +42,48 @@ const Header: FC = () => {
         </Link>
       </div>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost">
-            <User2 />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel asChild>
-            <div className="grid flex-1 text-left text-sm">
-              <span className="font-semibold">{session?.user?.name}</span>
-              <span className="text-xs text-gray-500">{session?.user?.email}</span>
-            </div>
-          </DropdownMenuLabel>
+      <div className="flex items-center gap-3">
+        <Select value={langValue} onValueChange={onChangeLang}>
+          <SelectTrigger className="w-[66px]">
+            <SelectValue>
+              <Image src={localesInfo[langValue].icon} alt={localesInfo[langValue].text} height={16} width={16} />
+            </SelectValue>
+          </SelectTrigger>
 
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={async () => await signOut()}>
-            <LogOut />
-            Log Out
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          <SelectContent>
+            <SelectGroup>
+              {locales.map((lang) => (
+                <SelectItem key={lang} value={lang}>
+                  <Image src={localesInfo[lang].icon} alt={localesInfo[lang].text} height={16} width={16} />
+                  {localesInfo[lang].text}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <User2 />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel asChild>
+              <div className="grid flex-1 text-left text-sm">
+                <span className="font-semibold">{session?.user?.name}</span>
+                <span className="text-xs text-gray-500">{session?.user?.email}</span>
+              </div>
+            </DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={async () => await signOut()}>
+              <LogOut />
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </header>
   );
 };
