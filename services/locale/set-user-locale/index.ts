04187fd48068685adf57f00f9ded9bc04e216ae8
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-06-20 01:15:02 +0300

    fix: i18n cookie options

--- services/locale/set-user-locale/index.ts
+++ services/locale/set-user-locale/index.ts
@@ -8,5 +8,10 @@ import { COOKIE_NAME } from '@/constants/locale';
 
 export async function setUserLocale(locale: Locale) {
   const cookiesStore = await cookies();
-  cookiesStore.set(COOKIE_NAME, locale);
+  cookiesStore.set(COOKIE_NAME, locale, {
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 365,
+    sameSite: 'lax',
+    path: '/',
+  });
 }
