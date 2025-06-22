c1d75403ede55d8ae8fa117bb1410137baeaf3e9
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-06-22 14:46:45 +0300

    feat: Seed global description settings for existing users
    - Adds a seeder to populate `description_settings_id` for all users
    - Ensures consistent default behavior after migration to user-level settings

--- prisma/seeders/seed.ts
+++ prisma/seeders/seed.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 import { createStatuses } from './create-statuses.seeder';
 import { createDescriptionSettings } from './create-description-settings.seeder';
+import { updateDescriptionSettingsForUser } from './update-description-settings-for-user.seeder';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await createStatuses();
   await createDescriptionSettings();
+  await updateDescriptionSettingsForUser();
 }
 
 main()
---
+++ prisma/seeders/update-description-settings-for-user.seeder.ts
@@ -0,0 +1,41 @@
+import { DESCRIPTION_SETTINGS_KEYS } from '@/constants/description-settings';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const updateDescriptionSettingsForUser = async () => {
+  const descriptionSettings = await prisma.descriptionSettings.findUnique({
+    where: {
+      value: DESCRIPTION_SETTINGS_KEYS.display_3_lines,
+    },
+  });
+
+  if (!descriptionSettings) {
+    console.log('There is no data on the description settings table');
+    return;
+  }
+
+  const users = await prisma.users.findMany({
+    where: {
+      description_settings_id: null,
+    },
+  });
+
+  if (users.length === 0) {
+    console.log('-> Done: All users have updated their settings.');
+    return;
+  }
+
+  const userIds = users.map((user) => user.id);
+
+  console.log(`Users with IDs: ${userIds} are waiting for the user settings to be updated`);
+
+  users.forEach(async (user) => {
+    await prisma.users.update({
+      where: { id: user.id },
+      data: { description_settings_id: descriptionSettings.id },
+    });
+  });
+
+  console.log('-> Done: All users have updated their settings.');
+};
