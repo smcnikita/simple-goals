0ba42bc5c644315aa5260f568af53d49c54b603a
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2025-07-21 18:29:25 +0300

    fix(user-delete): add data deletion via transaction, add section deletion (#34)

--- services/user/delete/service.ts
+++ services/user/delete/service.ts
@@ -1,31 +1,31 @@
 import { prisma } from '@/lib/prisma/prisma';
 
 export const deleteUser = async (id: number) => {
-  const useGoals = await prisma.goals.findMany({
-    where: {
-      user_id: id,
-    },
-  });
-
-  useGoals.forEach(async (goal) => {
-    await prisma.goals.delete({
-      where: { id: goal.id },
+  await prisma.$transaction(async (tx) => {
+    // Deleting the user's goals
+    await tx.goals.deleteMany({
+      where: { user_id: id },
     });
-  });
 
-  const userYears = await prisma.years.findMany({
-    where: {
-      user_id: id,
-    },
-  });
-
-  userYears.forEach(async (year) => {
-    await prisma.years.delete({
-      where: { id: year.id },
+    // Get all the user's years
+    const userYears = await tx.years.findMany({
+      where: { user_id: id },
     });
-  });
 
-  return await prisma.users.delete({
-    where: { id },
+    // Delete all sections and years
+    for (const year of userYears) {
+      await tx.section.deleteMany({
+        where: { years_id: year.id },
+      });
+
+      await tx.years.delete({
+        where: { id: year.id },
+      });
+    }
+
+    // Deleting the user
+    await tx.users.delete({
+      where: { id },
+    });
   });
 };
